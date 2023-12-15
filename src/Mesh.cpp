6d22b2d604de14164e220e225e357138c6a5cb0f
Phoenix Hollingsworth <phoenixjmh@gmail.com>
2023-12-15 15:01:21 -0700

    Logger
    Logger added

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,99 +1,79 @@
 #include "Mesh.h"
+using namespace Logger;
 
+void printTextureInfo(std::vector<Texture> textures)
+{
+    for (auto& tex : textures)
+    {
+        Log("Texture: \n","ID:",tex.id,"Path: ", tex.path,"Type: ", tex.type);
+    }
+}
 Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices,
     vector<Texture> textures)
     : Vertices(vertices)
     , Indices(indices)
     , Textures(textures)
-/* , m_vertexArray(nullptr)
- , m_vertexBuffer(nullptr)
- , m_indexBuffer(nullptr)*/
 {
+    Log("Mesh Class:","\nVertices found:",vertices.size(),"\nIndices found:",indices.size(),"\nTextures found:",textures.size());
+    printTextureInfo(textures);
     setupMesh();
 }
 
 void Mesh::Draw(Shader& shader)
 {
-    unsigned int diffuseNr = 1;
-    unsigned int specularNr = 1;
+    unsigned int diffuse_nr = 1;
+    unsigned int specular_nr = 1;
     unsigned int normalNr = 1;
     unsigned int heightNr = 1;
     for (auto i = 0; i < Textures.size(); i++) {
+        glActiveTexture(GL_TEXTURE0 + i);
         string number;
         string name = Textures[i].type;
+        string material_name;
         if (name == "texture_diffuse") {
-            number = std::to_string(diffuseNr++);
-            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+            material_name = "material.diffuse";
+            number = std::to_string(diffuse_nr++);
+            glUniform1i(glGetUniformLocation(shader.ID, (material_name + number).c_str()), i);
             glBindTexture(GL_TEXTURE_2D, Textures[i].id);
+        } else if (name == "texture_specular") {
+            material_name="material.specular";
+            number = std::to_string(specular_nr++);
+            glUniform1i(glGetUniformLocation(shader.ID, (material_name + number).c_str()), i);
+            glBindTexture(GL_TEXTURE_2D, Textures[i].id);
+            
         }
-        /* else if (name == "texture_specular")
-             number = std::to_string(specularNr++);
-         else if (name == "texture_normal")
-             number = std::to_string(normalNr++);
-         else if (name == "texture_height")
-             number = std::to_string(heightNr++);*/
+        
+             
     }
+ 
+       glActiveTexture(GL_TEXTURE0);
 
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(Indices.size()), GL_UNSIGNED_INT, 0);
+    m_vertexArray->Bind();
+    if (Indices.size() > 0)
+        glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(Indices.size()), GL_UNSIGNED_INT, 0);
+    else {
+        glDrawArrays(GL_TRIANGLES, 0, static_cast<unsigned int>(Vertices.size()));
+    }
 
     glBindVertexArray(0);
     glActiveTexture(GL_TEXTURE0);
-    // write access
 }
+
 void Mesh::setupMesh()
 {
-    // create buffers/arrays
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-    // load data into vertex buffers
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // A great thing about structs is that their memory layout is sequential for all its items.
-    // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-    // again translates to 3/2 floats which translates to a byte array.
-    glBufferData(GL_ARRAY_BUFFER, Vertices.size() * sizeof(Vertex), &Vertices[0], GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.size() * sizeof(unsigned int), &Indices[0], GL_STATIC_DRAW);
-
-    // set the vertex attribute pointers
-    // vertex Positions
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-    // vertex normals
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-    // vertex texture coords
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
-    //// vertex tangent
-    // glEnableVertexAttribArray(3);
-    // glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
-    //// vertex bitangent
-    // glEnableVertexAttribArray(4);
-    // glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
-    //// ids
-    // glEnableVertexAttribArray(5);
-    // glVertexAttribIPointer(5, 4, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, m_BoneIDs));
-
-    //// weights
-    // glEnableVertexAttribArray(6);
-    // glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_Weights));
-    glBindVertexArray(0);
+    Log("Initializing Mesh buffers");
+    if (Indices.size() > 0) {
+        VertexBuffer m_vertexBuffer(Vertices.data(), static_cast<unsigned int>(Vertices.size()));
+        IndexBuffer index_buffer(Indices, Indices.size());
+        m_vertexArray = new VertexArray(m_vertexBuffer);
+        index_buffer.Bind();
+        m_vertexArray->Unbind();
+        m_vertexBuffer.UnBind();
+    } else {
+        Log("No Indices found, proceeding without IndexBuffer");
+        VertexBuffer m_vertexBuffer(Vertices.data(), static_cast<unsigned int>(Vertices.size()));
+        m_vertexArray = new VertexArray(m_vertexBuffer);
+        m_vertexArray->Unbind();
+        m_vertexBuffer.UnBind();
+    }
 }
-
-// void Mesh::setupMesh()
-//{
-//     m_vertexBuffer = new VertexBuffer(Vertices.data(), static_cast<unsigned int>(Vertices.size()));
-//     IndexBuffer index_buffer(Indices, Indices.size());
-//     m_vertexArray = new VertexArray();
-//     m_vertexArray->AddBuffer(m_vertexBuffer);
-//     //m_indexBuffer->Bind();
-//     index_buffer.Bind();
-//     m_vertexArray->Unbind();
-//     m_vertexBuffer->UnBind();
-//     delete m_vertexBuffer;
-// }
--- src/Mesh.h
+++ src/Mesh.h
@@ -1,35 +1,31 @@
 #pragma once
 #include "IndexBuffer.h"
+#include "Logger.h"
 #include "Shader.h"
 #include "VertexArray.h"
 #include "glm/glm.hpp"
 #include <string>
 #include <vector>
-
 using std::string;
 using std::vector;
+using namespace Logger;
 
- struct Texture{
-   unsigned int id;
-   string type;
-   string path;
- };
+struct Texture {
+    unsigned int id;
+    string type;
+    string path;
+};
 
 class Mesh {
 public:
     Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
-
     std::vector<Vertex> Vertices;
     std::vector<unsigned int> Indices;
     std::vector<Texture> Textures;
-
     void Draw(Shader& shader);
     unsigned int VAO;
 
 private:
-  /*  VertexArray* m_vertexArray;
-    VertexBuffer* m_vertexBuffer;
-    IndexBuffer* m_indexBuffer;*/
-    unsigned int VBO,EBO;
+    VertexArray* m_vertexArray;
     void setupMesh();
 };
